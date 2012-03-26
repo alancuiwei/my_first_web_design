31bfa1e0da4cf00d19f713a3a4b6a8f594fb5a70
alancuiwei <alan_cuiwei@yahoo.com.cn>
2012-03-26 08:54:21 +0800

    brower IE caparity

--- db/migrate/20120314021358_add_strategyweb_id_to_webuserstrategy.rb
+++
@@ -1,8 +0,0 @@
-class AddStrategywebIdToWebuserstrategy < ActiveRecord::Migration
-  def self.up
-	add_column :webuserstrategy, :strategyweb_id, :integer
-  end
-  def self.down
-	remove_column :webuserstrategy, :strategyweb_id
-  end
-end
