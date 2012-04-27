9b4b66f53f8c529919647f1f9afe84016f1356dc
alancuiwei <alan_cuiwei@yahoo.com.cn>
2012-04-27 09:04:50 +0800

    28501643 done

--- app/controllers/usermanagement_controller.rb
+++ app/controllers/usermanagement_controller.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 require 'rexml/document'
 #require 'rexml/streamlistener'
 include REXML
@@ -14,7 +15,12 @@ def usertradecharge
    @webuser = Webuser.find_by_name(session[:webuser_name])
    #取得默认值
    @usercommodity=UsercommodityT.find_all_by_userid(session[:webuser_name])
-
+  #
+  @hash_commodityid=Hash["CF","棉花","ER","早籼稻","ME","甲醇","RO","菜籽油","SR","白糖","TA","精对苯二甲酸","WS","强麦",
+       "WT","硬麦","PM","普麦","IF","股指","a","黄大豆","al","铝","au","黄金","b","黄豆二","c","玉米",
+       "cu","铜","fu","燃料油","j","焦炭","l","聚乙烯","m","豆粕","p","棕榈油","pb","铅","rb","螺纹钢",
+       "ru","橡胶","v","聚氯乙烯","wr","线材","y","豆油","zn","锌"]
+   @hash_tradechargetype=Hash[0,"每手",1,"成交金额比例"]
 end
 def userlendrate
   #session
