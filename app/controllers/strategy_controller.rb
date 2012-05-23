77f946b47498541490a445b5f803387daacd8d66
alancuiwei <alan_cuiwei@yahoo.com.cn>
2012-05-23 17:27:52 +0800

    29508405

--- app/controllers/strategy_controller.rb
+++ app/controllers/strategy_controller.rb
@@ -14,18 +14,7 @@ class StrategyController < ApplicationController
     render :json => @gettime
     end
     session[:login]="shownorisk"
-    @stg010001 = Stg010001.find_all_by_username(session[:webuser_name])
-    if params[:pairname]!=nil
-      Stg010001.new do |stg|
-        stg.username=session[:webuser_name]
-        stg.pairname=params[:pairname]
-        stg.returnrate=params[:returnrate].to_f
-        stg.time=Time.now.to_s(:db)
-        stg.save
-      end
-    end
     @webuser = Webuser.find_by_name(session[:webuser_name])
-    @strategyparam = StrategyparamT.find_by_username_and_strategyid_and_paramname(session[:webuser_name],"010001","returnrate")
     @defaultusercommodity=UsercommodityT.find_all_by_userid("tester1")
     #default from db
     defaultdb=Struct.new(:commodityid,:lendrate,:tradecharge,:trademargingap)
@@ -83,6 +72,81 @@ class StrategyController < ApplicationController
   def personaltrading
     session[:login]="personaltrading"
     @webuser = Webuser.find_by_name(session[:webuser_name])
+
+    ####
+    if params[:gettime_p]!=nil
+        @gettime_p=Time.now
+        puts @gettime_p
+        render :json => @gettime_p
+        end
+    @stg010001 = Stg010001.find_all_by_username(session[:webuser_name])
+        if params[:pairname_p]!=nil
+      Stg010001.new do |stg|
+        stg.username=session[:webuser_name]
+            stg.pairname=params[:pairname_p]
+            stg.returnrate=params[:returnrate_p].to_f
+        stg.time=Time.now.to_s(:db)
+            stg.firstprice=params[:firstprice_p]
+            stg.secondprice=params[:secondprice_p]
+            stg.firstmarginrate=params[:firstmarginrate_p]
+            stg.secondmarginrate=params[:secondmarginrate_p]
+        stg.save
+      end
+    end
+    @webuser = Webuser.find_by_name(session[:webuser_name])
+    @strategyparam = StrategyparamT.find_by_username_and_strategyid_and_paramname(session[:webuser_name],"010001","returnrate")
+    @defaultusercommodity=UsercommodityT.find_all_by_userid("tester1")
+    #default from db
+    defaultdb=Struct.new(:commodityid,:lendrate,:tradecharge,:trademargingap)
+    @defaultdb=Array.new
+    for i in 0..@defaultusercommodity.size-1 do
+      @defaultdb[i]=defaultdb.new(@defaultusercommodity[i].commodityid,@defaultusercommodity[i].lendrate,@defaultusercommodity[i].tradecharge,@defaultusercommodity[i].trademargingap)
+    end
+    @db_num=@defaultusercommodity.size
+    #login flag
+    @userflag=0
+    #for default 初始化
+    #just for chargetrade edit
+    userchange=Struct.new(:commodityid,:tradecharge)
+    @userchange=Array.new
+    @tradechargechange_num=0
+    #user from db
+    userdb=Struct.new(:commodityid,:lendrate,:trademargingap)
+    @userdb=Array.new
+    for i in 0..@defaultusercommodity.size-1 do
+         @userdb[i]=userdb.new(@defaultusercommodity[i].commodityid,@defaultusercommodity[i].lendrate,@defaultusercommodity[i].trademargingap)
+       end
+    if @webuser!=nil
+      #login
+      @userflag=1
+    @usercommodity=UsercommodityT.find_all_by_userid(@webuser.name)
+    user_tradecharge=Array.new
+    #重组数组
+      #change tradecharge commodityid
+    useredit_commodityid=Array.new
+      #get all form db
+   for i in 0..@usercommodity.size-1 do
+      user_tradecharge[i]=@usercommodity[i].tradecharge
+     @userdb[i]=userdb.new(@usercommodity[i].commodityid,@usercommodity[i].lendrate,@usercommodity[i].trademargingap)
+   end
+    for i in 0..@usercommodity.size-1 do
+      useredit_commodityid[i]=-1
+        if( user_tradecharge[i]!=@defaultdb[i].tradecharge)
+          useredit_commodityid[i]=@userdb[i].commodityid
+          end
+    end
+    j=0
+    for i in 0..@usercommodity.size-1 do
+          if(useredit_commodityid[i]!=-1)
+        @userchange[j]=userchange.new(useredit_commodityid[i],user_tradecharge[i])
+            j=j+1
+          end
+    end
+      @tradechargechange_num=j
+    end #end if
+
+    ####
+
   end
 
   def maxreturnrate
