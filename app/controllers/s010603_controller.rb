12851379517254b7ee1e03b405c43727dc7911b5
alancuiwei <alan_cuiwei@yahoo.com.cn>
2012-06-05 17:19:59 +0800

    30692675

--- app/controllers/s010603_controller.rb
+++ app/controllers/s010603_controller.rb
@@ -7,7 +7,6 @@ class S010603Controller < ApplicationController
     @streference = StrategyreferenceT.find_all_by_rightid("010603000000")
     @strategyreturnrates=StrategyreturnrateT.find_all_by_rightid("010603000000")
     @returnrate_arr=Array.new
-
     if params[:getjson]!=nil
       i=0
          @strategyreturnrates.each do |stgreturnrate|
@@ -17,8 +16,40 @@ class S010603Controller < ApplicationController
          end
     render :json => @returnrate_arr #render json #render json
     end
+    @profits=StrategypositionrecordT.all
+    @hash_profit=Hash.new
+    @profit_arr=Array.new
 
+    for i in 0..3455-1
+      @hash_profit.store(1026777600000+i*86400000,0)
+    end
 
+      @profits.each do |profit|
+        if @hash_profit[DateTime.strptime(profit.closeposdate.to_s(:db), "%Y-%m-%d").to_i*1000]==0
+           @hash_profit.store(DateTime.strptime(profit.closeposdate.to_s(:db), "%Y-%m-%d").to_i*1000,profit.profit)
+        else
+          @profit_temp=@hash_profit[DateTime.strptime(profit.closeposdate.to_s(:db), "%Y-%m-%d").to_i*1000]+profit.profit
+          @hash_profit.store(DateTime.strptime(profit.closeposdate.to_s(:db), "%Y-%m-%d").to_i*1000,@profit_temp)
+        end
+      end
+
+    for i in 0..3455-1
+      if @hash_profit[1026777600000+i*86400000]==0
+      @hash_profit.store(1026777600000+i*86400000,@hash_profit[@hash_profit.keys[i-1]])
+      end
+    end
+    for i in 0..3455-1
+      if @hash_profit[1026777600000+(i-1)*86400000]!=nil
+        @hash_profit.store(1026777600000+i*86400000,@hash_profit[@hash_profit.keys[i]]+@hash_profit[@hash_profit.keys[i-1]])
+        end
+    end
+
+    if params[:getprofit]!=nil
+     for i in 0..@hash_profit.keys.length-1
+       @profit_arr[i]=[@hash_profit.keys[i],200000+@hash_profit[@hash_profit.keys[i]]]
+     end
+     render :json => @profit_arr #render json #render json
+    end
   end
 
   def showall
