3b61804099055f0858a1f63ccdb02a0244798b68
alancuiwei <alan_cuiwei@yahoo.com.cn>
2012-06-17 13:45:42 +0800

    31116797

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
   def index
     @versions=Versionstable.find(:all, :order =>"update_date DESC",:limit => 3)
     @versions_all=Versionstable.find(:all, :order =>"update_date DESC")
+    @strategyweb=Strategyweb.find(:all, :order =>"updated_at DESC",:limit => 3)
+    @strategyweb_all=Strategyweb.find(:all, :order =>"updated_at DESC")
   end
   def comingsoon
   end
