0e50064f08d7a0a3ec3d04cffe2315e79640d996
alancuiwei <alan_cuiwei@yahoo.com.cn>
2012-06-25 10:40:03 +0800

    31050771

--- db/migrate/20120413081342_create_usercommodity_t.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsercommodityT < ActiveRecord::Migration
-  def change
-    create_table :usercommodity_t do |t|
-      t.float :tardecharge
-
-      t.timestamps
-    end
-  end
-end
---
+++ db/migrate/20120621060639_creat.rb
@@ -0,0 +1,9 @@
+class Creat < ActiveRecord::Migration
+  def change
+      create_table :categories do |t|
+        t.string :name
+        t.integer :position
+        t.timestamps
+      end
+  end
+end
