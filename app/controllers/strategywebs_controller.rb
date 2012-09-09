71b1252341749842ae96993ce19509836e93544e
alancuiwei <alan_cuiwei@yahoo.com.cn>
2012-09-09 16:36:11 +0800

    35726435 & 35727001

--- app/controllers/strategywebs_controller.rb
+++ app/controllers/strategywebs_controller.rb
@@ -129,6 +129,21 @@ class StrategywebsController < ApplicationController
     end
   end
   
+  def stgwebdata
+    @commoditys=CommodityT.all
+    @hash_commoditys={}
+    @hash_commodityid=Hash["ag","白银","CF","棉花","ER","早籼稻","ME","甲醇","RO","菜籽油","SR","白糖","TA","精对苯二甲酸","WS","强麦",
+         "WT","硬麦","PM","普麦","IF","股指","a","黄大豆","al","铝","au","黄金","b","黄豆二","c","玉米",
+         "cu","铜","fu","燃料油","j","焦炭","l","聚乙烯","m","豆粕","p","棕榈油","pb","铅","rb","螺纹钢",
+         "ru","橡胶","v","聚氯乙烯","wr","线材","y","豆油","zn","锌"]
+    @hash_city=Hash["DL","大连","ZZ","郑州","SH","上海"]
+    for i in 0..@commoditys.size-1
+      @hash_commoditys.store(@commoditys[i].commodityid,@hash_city[@commoditys[i].exchangeid])
+    end
+  end
+  def download
+    send_file "app/assets/xmls/"+params[:filename] unless params[:filename].blank?
+  end
   # GET /strategywebs/1
   # GET /strategywebs/1.json
   def show
