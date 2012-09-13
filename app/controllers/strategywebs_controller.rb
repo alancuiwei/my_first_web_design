9ab2a6f4e50c6ff042b1b28f0c26743e38ec3cd2
alancuiwei <alan_cuiwei@yahoo.com.cn>
2012-09-13 15:17:17 +0800

    35727001

--- app/controllers/strategywebs_controller.rb
+++ app/controllers/strategywebs_controller.rb
@@ -132,11 +132,11 @@ class StrategywebsController < ApplicationController
   def stgwebdata
     @commoditys=CommodityT.all
     @hash_commoditys={}
-    @hash_commodityid=Hash["ag","白银","CF","棉花","ER","早籼稻","ME","甲醇","RO","菜籽油","SR","白糖","TA","精对苯二甲酸","WS","强麦",
+    @hash_commodityid=Hash["ag","白银","CF","棉花","ER","早籼稻(改名前)","RI","早籼稻(改名后)","ME","甲醇","RO","菜籽油(改名前)","OI","菜籽油(改名后)","SR","白糖","TA","精对苯二甲酸","WS","强麦(改名前)","WH","强麦(改名后)",
          "WT","硬麦","PM","普麦","IF","股指","a","黄大豆","al","铝","au","黄金","b","黄豆二","c","玉米",
          "cu","铜","fu","燃料油","j","焦炭","l","聚乙烯","m","豆粕","p","棕榈油","pb","铅","rb","螺纹钢",
          "ru","橡胶","v","聚氯乙烯","wr","线材","y","豆油","zn","锌"]
-    @hash_city=Hash["DL","大连","ZZ","郑州","SH","上海"]
+    @hash_city=Hash["DL","大连","ZZ","郑州","SH","上海","ZJS","中金所"]
     for i in 0..@commoditys.size-1
       if FileTest::exist?'app/assets/historydatadownload/'+@commoditys[i].commodityid+'.zip'
         date=File::mtime('app/assets/historydatadownload/'+@commoditys[i].commodityid+'.zip').to_s(:db)
