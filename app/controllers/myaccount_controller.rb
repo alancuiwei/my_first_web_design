ce734f720cf51ed05c1245497ab1ba4532e741ad
alancuiwei <alan_cuiwei@yahoo.com.cn>
2012-11-21 17:39:48 +0800

    hefeng firstly implementation

--- app/controllers/myaccount_controller.rb
+++ app/controllers/myaccount_controller.rb
@@ -1,2 +1,17 @@
+require 'date'
 class MyaccountController < ApplicationController
+  def investrecord
+    @funds=Investrecord.find_all_by_username_and_recordtype("feifan","fund")
+    @interests=Investrecord.find_all_by_username_and_recordtype("feifan","interest")
+    @allfund=0
+    @funds.each do |f|
+      @allfund=@allfund+f.recordvalue
+    end
+    @allinterest=0
+    @interests.each do |i|
+      @allinterest=@allinterest+i.recordvalue
+    end
+    @allinvest=@allfund+@allinterest
+
+  end
 end
