e1fd09fe5160e62446459d1474be2348b161f900
alancuiwei <alan_cuiwei@yahoo.com.cn>
2012-11-27 17:41:22 +0800

    1127 code update 2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 require 'date'
 class ProductsController < ApplicationController
   Time::DATE_FORMATS[:stamp] = '%Y-%m-%d'
@@ -6,6 +7,21 @@ class ProductsController < ApplicationController
 
     @invest_f=Investrecord.find_all_by_recordtype("fund")
     @invest_i=Investrecord.find_all_by_recordtype("interest")
+    piefunds={}
+    @pie={}
+    @webusers=Webuser.all
+    @webusers.each do |w|
+      piefunds.store(w.username,[Investrecord.find_all_by_username_and_recordtype(w.username,"fund")])
+    end
+    for i in 0..@webusers.size-1
+
+      userfund=piefunds[@webusers[i].username][0]
+      pie=0
+      for j in 0..userfund.size-1
+        pie=pie+userfund[j].recordvalue
+      end
+      @pie.store(@webusers[i].username,pie)
+    end
     @funds=0
     @invest_f.each do |i|
       @funds=@funds+i.recordvalue
