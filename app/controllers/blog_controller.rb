b2e662f1dd508b628a9d1df4dd512eeb67ec163b
alancuiwei <alan_cuiwei@yahoo.com.cn>
2012-12-21 15:27:49 +0800

    1221 code update 2

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -3,4 +3,25 @@ class BlogController < ApplicationController
   def index
     @blogs=Blog.find_by_sql('select * from blog order by id desc')
   end
+
+  def blogarticle
+    @bloginfo=[]
+    if params[:id]!="0"
+      @blog=Blog.find_by_id(params[:id])
+      @bloginfo[0]=@blog.btitle
+      @bloginfo[1]=@blog.blname
+      @bloginfo[2]=@blog.publishdate
+      @bloginfo[3]=@blog.barticle
+      @bloginfo[4]=@blog.bcolumn
+      @bloginfo[5]=@blog.imagepath
+    end
+  end
+
+  def blogbusinesslife
+    @blogs=Blog.find_by_sql('select * from blog where bcolumn="创业人生" order by id desc')
+  end
+
+  def blogbankfinance
+    @blogs=Blog.find_by_sql('select * from blog where bcolumn="银行理财产品" order by id desc')
+  end
 end
