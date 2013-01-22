c6bfbf660e1b91428ccb0fca3788784d35fc810d
alancuiwei <alan_cuiwei@yahoo.com.cn>
2013-01-22 17:51:08 +0800

    0122 code update 2

--- app/assets/javascripts/placeholder.js
+++ app/assets/javascripts/placeholder.js
@@ -5,40 +5,84 @@
  * Time: 上午11:40
  * To change this template use File | Settings | File Templates.
  */
-//placeholder IE8
-var _placeholderSupport = function() {
-    var t = document.createElement("input");
-    t.type = "text";
-    return (typeof t.placeholder !== "undefined");
-}();
+$.fn.extend({
+    /**
+     * 该方法为了解决不支持placeholder属性的浏览器下达到相似效果作用
+     * @param _color 文本框输入字体颜色,默认黑色
+     * @param _plaColor 文本框placeholder字体颜色，默认灰色#a3a3a3
+     */
+    inputTip:function(_color, _plaColor) {
+        _color = _color || "#000000";
+        _plaColor = _plaColor || "#a3a3a3";
+        function supportsInputPlaceholder() { // 判断浏览器是否支持html5的placeholder属性
+            var input = document.createElement('input');
+            return "placeholder" in input;
+        }
 
-window.onload = function() {
-    var arrInputs = document.getElementsByTagName("input");
-    for (var i = 0; i < arrInputs.length; i++) {
-        var curInput = arrInputs[i];
-        if (!curInput.type || curInput.type == "" || curInput.type == "text")
-            HandlePlaceholder(curInput);
+        function showPassword(_bool, _passEle, _textEle) { // 密码框和文本框的转换
+            if (_bool) {
+                _passEle.show();
+                _textEle.hide();
+            } else {
+                _passEle.hide();
+                _textEle.show();
     }
-};
+        }
 
-function HandlePlaceholder(oTextbox) {
-    if (!_placeholderSupport) {
-        var curPlaceholder = oTextbox.getAttribute("placeholder");
-        if (curPlaceholder && curPlaceholder.length > 0) {
-            oTextbox.value = curPlaceholder;
-            oTextbox.setAttribute("old_color", oTextbox.style.color);
-            oTextbox.style.color = "#c0c0c0";
-            oTextbox.onfocus = function() {
-                this.style.color = this.getAttribute("old_color");
-                if (this.value === curPlaceholder)
-                    this.value = "";
-            };
-            oTextbox.onblur = function() {
-                if (this.value === "") {
-                    this.style.color = "#c0c0c0";
-                    this.value = curPlaceholder;
+        if (!supportsInputPlaceholder()) {
+            this.each(function() {
+                var thisEle = $(this);
+                var inputType = thisEle.attr("type")
+                var isPasswordInput = inputType == "password";
+                var isInputBox = inputType == "password" || inputType == "text";
+                if (isInputBox) { //如果是密码或文本输入框
+                    var isUserEnter = false; // 是否为用户输入内容,允许用户的输入和默认内容一样
+                    var placeholder = thisEle.attr("placeholder");
+
+                    if (isPasswordInput) { // 如果是密码输入框
+//原理：由于input标签的type属性不可以动态更改，所以要构造一个文本input替换整个密码input
+                        var textStr = "<input type='text' class='" + thisEle.attr("class") + "' style='" + (thisEle.attr("style") || "") + "' />";
+                        var textEle = $(textStr);
+                        textEle.css("color", _plaColor).val(placeholder).focus(
+                            function() {
+                                thisEle.focus();
+                            }).insertAfter(this);
+                        thisEle.hide();
                 }
+                    thisEle.css("color", _plaColor).val("");//解决ie下刷新无法清空已输入input数据问题
+                    if (thisEle.val() == "") {
+                        thisEle.val(placeholder);
             }
+                    thisEle.focus(function() {
+                        if (thisEle.val() == placeholder && !isUserEnter) {
+                            thisEle.css("color", _color).val("");
+                            if (isPasswordInput) {
+                                showPassword(true, thisEle, textEle);
         }
     }
+                    });
+                    thisEle.blur(function() {
+                        if (thisEle.val() == "") {
+                            thisEle.css("color", _plaColor).val(placeholder);
+                            isUserEnter = false;
+                            if (isPasswordInput) {
+                                showPassword(false, thisEle, textEle);
+                            }
+                        }
+                    });
+                    thisEle.keyup(function() {
+                        if (thisEle.val() != placeholder) {
+                            isUserEnter = true;
+                        }
+                    });
 }
+            });
+        }
+    }
+});
+
+
+$(function() {
+    $("input").inputTip();  // 调用inputTip方法
+    $("input[type='button']").focus();  // 页面打开后焦点置于button上，也可置于别处。否则IE上刷新页面后焦点在第一个输入框内造成placeholder文字后紧跟光标现象
+});
