167e1d0bf09dfae4066973d5f3c758cf033de5b6
alancuiwei <alan_cuiwei@yahoo.com.cn>
2013-02-22 16:00:19 +0800

    code update 0222 1

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -16,26 +16,6 @@ class SalesController < ApplicationController
     end
     end
 
-  def reserve
-    if  params[:investamount]==""
-      render :json=>'购买金额为空！'.to_json
-    elsif  params[:email]==""
-      render :json=>'电子邮箱为空！'.to_json
-    elsif  params[:tel]==""
-      render :json=>'联系电话为空！'.to_json
-    elsif  params[:card]==""  &&  params[:isuser]=="是"
-      render :json=>'银行卡号为空！'.to_json
-    elsif  params[:bankbranch]=="" &&  params[:isuser]=="是"
-      render :json=>'分行名为空！'.to_json
-    elsif  params[:investamount].to_i<params[:startvalue].to_i
-      render :json=>'购买金额不足'.to_json
-    elsif /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.match(params[:email])==nil
-      render :json=>'请输入正确的电子邮箱！'.to_json
-    else
-      render :json => "s".to_json
-    end
-  end
-
   def myorder
       @reserve=Reserve.find_all_by_username(session[:webusername])
     end
