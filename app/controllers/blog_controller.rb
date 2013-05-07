a4c631fc2d39ba57dd968c74d90351ae7fb5378b
alancuiwei <alan_cuiwei@yahoo.com.cn>
2013-05-07 12:52:17 +0800

    0507 code update 2

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -30,6 +30,21 @@ class BlogController < ApplicationController
     end
   end
 
+  def article
+    @bloginfo=[]
+    @comments=Comments.order("id desc").find_all_by_bid(params[:id])
+    if params[:id]!="0"
+      @blog=Blog.find_by_id(params[:id])
+      @bloginfo[0]=@blog.btitle
+      @bloginfo[1]=@blog.blname
+      @bloginfo[2]=@blog.publishdate
+      @bloginfo[3]=@blog.barticle
+      @bloginfo[4]=@blog.bcolumn
+      @bloginfo[5]=@blog.imagepath
+      @bloginfo[6]=@blog.id
+    end
+  end
+
   def commentconfigajax
     Comments.new do |b|
       b.username=params[:username]
