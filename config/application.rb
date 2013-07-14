1b003d262234d2409619bb431fb57ab47f0fb59b
alancuiwei <alan_cuiwei@yahoo.com.cn>
2013-07-14 13:53:36 +0800

    0714 code update 1

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,7 @@ module Ver01
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-  end
+
+    config.weixin_token = "echotest"  
+end
 end
