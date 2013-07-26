1387e37ef605dcd0318126c848eb8b1c6fcc23df
alancuiwei <alan_cuiwei@yahoo.com.cn>
2013-07-26 11:27:28 +0800

    0726 code update 1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,6 +10,7 @@ class HomeController < ApplicationController
     if session[:webusername]!=nil
       @webuser=Webuser.find_by_username(session[:webusername])
       @examination=Examination.find_by_username(session[:webusername])
+      @personal=Personalfinance.find_by_username(session[:webusername])
     end
     @category1=Category_2.find_all_by_risklevel(1)
     @category2=Category_2.find_all_by_risklevel(2)
