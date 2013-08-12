ceedfb0b67bc9467c1f997da238fb1654baa7ead
alancuiwei <alan_cuiwei@yahoo.com.cn>
2013-08-12 10:45:02 +0800

    0812 code update 2

---
+++ app/assets/download/weixin_menu.rb
@@ -0,0 +1,28 @@
+﻿#encoding:utf-8
+require "uri"
+require "net/http"
+require 'json'
+require 'open-uri'  
+
+# 获取token
+uri = URI.parse("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxe539a024b85dea80&secret=d8d7aa23299971093499b5af3ab79da0")
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+request = Net::HTTP::Get.new(uri.request_uri)
+response = http.request(request)
+array = JSON.parse(response.body)
+token = array["access_token"]
+puts token
+
+# 设置菜单
+params = {"button"=>[{"type"=>"click","name"=>"我们的服务","key"=>"V100"},{"type"=>"click","name"=>"理财知识","key"=>"V200"},{"name"=>"关于通天顺","sub_button"=>[{"type"=>"click","name"=>"二级菜单31","key"=>"V301"},{"type"=>"click","name"=>"二级菜单32","key"=>"V302"}]}]}  
+http = Net::HTTP.new("api.weixin.qq.com")
+request = Net::HTTP::Post.new("/cgi-bin/menu/create?access_token="+token,{'Content-Type' => 'application/json'})
+#request.set_form_data(params)
+request.body  = params.to_json
+response = http.request(request)
+
+puts response
+#puts uri
+puts params.to_json
