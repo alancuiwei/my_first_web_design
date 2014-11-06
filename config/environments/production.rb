3518ccf6774fb2c2a3b94a4b257f73ac96515e40
gagagithub <firefox.cui@gmail.com>
2014-11-06 09:37:13 +0800

    it works 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,8 @@ Ver01::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  config.eager_load = true
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
