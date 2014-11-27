6ebf343bc4724e28466125494810ab32bc006049
gagagithub <firefox.cui@gmail.com>
2014-11-27 14:23:27 +0800

    online web url update for pdfkit

---
+++ config/initializers/pdfkit.rb
@@ -0,0 +1,12 @@
+# config/initializers/pdfkit.rb
+PDFKit.configure do |config|
+#  config.wkhtmltopdf = '/Users/alan_cuiwei/workspace/to/wkhtmltopdf'
+  config.default_options = {
+    :page_size => 'Legal',
+    :print_media_type => true
+  }
+  # Use only if your external hostname is unavailable on the server.
+  config.root_url = "http://www.tongtianshun.com"
+  config.verbose = false
+
+end
